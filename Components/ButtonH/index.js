148071cf15cfe3f6b6015bb4c8e0f7bf2fe3ce73
IngridChou <ingridchoudj@gmail.com>
2023-04-10 02:17:52 -0700

    changing and adding stuffs

--- Components/ButtonH/index.js
+++ Components/ButtonH/index.js
@@ -9,17 +9,17 @@ export default function ButtonH() {
             <div className={styles.container}>
                 <Link className={styles.link} href="/reflection">
                     <button className={styles.intro}>Introduction
-                        <Image src="/arrow.svg" alt="Drug Image" width={35} height={35} />
+                        <Image src="/arrow.svg" alt="Arrow" width={35} height={35} />
                     </button>
                 </Link>
                 <Link className={styles.link} href="/quiz">
                     <button className={styles.quiz}>Take a Quiz
-                        <Image src="/arrow.svg" alt="Drug Image" width={35} height={35} />
+                        <Image src="/arrow.svg" alt="Arrow" width={35} height={35} />
                     </button>
                 </Link>
                 <Link className={styles.link} href="/about">
                     <button className={styles.about}>About Us
-                        <Image src="/arrow.svg" alt="Drug Image" width={35} height={35} />
+                        <Image src="/arrow.svg" alt="Arrow" width={35} height={35} />
                     </button>
                 </Link>
             </div>
--- Components/ButtonR/index.js
+++ Components/ButtonR/index.js
@@ -7,7 +7,7 @@ export default function ButtonR() {
         <>
             <Link className={styles.link} href="/quiz">
                 <button className={styles.button}>I want to learn more
-                    <Image src="/arrow.svg" alt="Drug Image" width={35} height={35} />
+                    <Image src="/arrow.svg" alt="Arrow" width={35} height={35} />
                 </button>
             </Link>
         </>
