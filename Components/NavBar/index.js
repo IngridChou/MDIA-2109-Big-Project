be866eff372c8933f05e9a5d2e5b7c2c3befc80a
wendyychenn <113089770+wendyychenn@users.noreply.github.com>
2023-04-14 09:34:53 -0700

    Update index.js

--- Components/NavBar/index.js
+++ Components/NavBar/index.js
@@ -8,13 +8,13 @@ export default function NavBar() {
     return (
         <>
             <div className={styles.main}>
-                <Link href='/quiz.js'>
+                <Link href='/quiz'>
                     <Image src="/quiz-icon.png" width={36} height={36} />
                 </Link>
                 <Link href='/'>
                     <Image src="/home.png" width={36} height={36} />
                 </Link>
-                <Link href='/resources.js'>
+                <Link href='/resources'>
                     <Image src="/info-icon.png" width={36} height={36} />
                 </Link>
             </div>
