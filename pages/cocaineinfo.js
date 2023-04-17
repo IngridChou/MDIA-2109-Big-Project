8cee15bed591ae3e8be19d016dc2a4013e87e517
IngridChou <113267284+IngridChou@users.noreply.github.com>
2023-04-17 14:59:59 -0700

    Update cocaineinfo.js

--- pages/cocaineinfo.js
+++ pages/cocaineinfo.js
@@ -27,7 +27,7 @@ export default function Cocaineinfo() {
 
         <h2 className={styles.subheading}>Precautions</h2>
         <p className={styles.content}>&#x2022; Only take cocaine if you are in good physical and <br />mental health<br />&#x2022; Always test your cocaine before taking it <br />&#x2022; Stay hydrated, but dont drink too much water <br />&#x2022; Do not drive or operate heavy machinery while under <br /> the influence of cocaine</p>
-        <MoreInfo/>
+        /**<MoreInfo/>*/
         <NavBar />
         </main>
 </>
