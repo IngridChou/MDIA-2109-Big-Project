c27c8dd4f86b08705ba8e70c32725acffa2a2210
IngridChou <ingridchoudj@gmail.com>
2023-04-17 09:22:07 -0700

    fixing vercel error

--- pages/alcohol.js
+++ pages/alcohol.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/alcohol.module.css'
-import PrevButton from '..COmponents/PrevButton'
+import PrevButton from '../Components/PrevButton'
 import NavBar from '../Components/NavBar'
 
 export default function Alcohol() {
