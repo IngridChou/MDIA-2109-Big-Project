271d467a5e3ed2489ca11e8889f27e8ca008f114
paulsheo <113318941+paulsheo@users.noreply.github.com>
2023-04-17 13:40:14 -0700

    page set up

---
+++ pages/alcoholinfo.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/alcohol.module.css'
+import PrevButton from '../Components/PrevButton'
+import NavBar from '../Components/NavBar'
+
+export default function Alcoholinfo() {
+    return (
+
+
+
+
+
+    )
+    }
