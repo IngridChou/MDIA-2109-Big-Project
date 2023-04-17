bfb8a968c027d14721128bac22a6f65e4ac0f345
paulsheo <113318941+paulsheo@users.noreply.github.com>
2023-04-17 14:02:02 -0700

    media screen

--- pages/cocaineinfo.js
+++ pages/cocaineinfo.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/alcohol.module.css'
+import styles from '../styles/cocaineinfo.module.css'
 import PrevButton from '../Components/PrevButton'
 import NavBar from '../Components/NavBar'
 
