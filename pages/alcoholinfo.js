80ca3a479a77419aa7dd9ead93ee0767eba5e26e
wendyychenn <wchen242@my.bcit.ca>
2023-04-24 06:31:43 -0700

    fixing errors

--- pages/alcoholinfo.js
+++ pages/alcoholinfo.js
@@ -22,10 +22,10 @@ export default function Alcoholinfo() {
         <p clsasName={styles.content}>Cocaine is a highly addictive stimulant drug that is<br />derived from the leaves of the coca plant. Cocaine <br />works by blocking the reabsorption of dopamine</p>
 
         <h2 className={styles.subheading}>Effects</h2>
-        <p className={styles.content}>&#x2022; Feelings of euphoria, happiness, and increased <br /> heartbeat <br />&#x2022; Pupils of the eyes dilate, making them larger <br />than normal <br />&#x2022; Cocaine makes it difficult to sleep, leading to <br />insomnia</p>
+        <p className={styles.content}>Feelings of euphoria, happiness, and increased <br /> heartbeat <br />Pupils of the eyes dilate, making them larger <br />than normal <br /> Cocaine makes it difficult to sleep, leading to <br />insomnia</p>
 
         <h2 className={styles.subheading}>Precautions</h2>
-        <p className={styles.content}>&#x2022; Only take cocaine if you are in good physical and <br />mental health<br />&#x2022; Always test your cocaine before taking it <br />&#x2022; Stay hydrated, but dont drink too much water <br />&#x2022; Do not drive or operate heavy machinery while under <br /> the influence of cocaine</p>
+        <p className={styles.content}>Only take cocaine if you are in good physical and <br />mental health<br /> Always test your cocaine before taking it <br />Stay hydrated, but dont drink too much water <br />Do not drive or operate heavy machinery while under <br /> the influence of cocaine</p>
     
         <NavBar />
         </main>
