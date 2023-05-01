702869752f3412d1bbb5b712ac781f2d85ab0caf
wendyychenn <wchen242@my.bcit.ca>
2023-04-30 20:38:41 -0700

    adding graph

---
+++ Components/Graph/index.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import styles from './Graph.module.css'
+
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default function Graph() {
+    
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: [2017, 2018, 2019, 2020, 2021],
+            datasets: [
+                {
+                    label: 'Number of People',
+                    data: [94300, 77100, 62100, 66500, 81000],
+                    borderColor: 'rgb(53,162,235)',
+                    backgroundColor: 'rgba(53,162,235,0.4)'
+                }
+            ]
+        })
+
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: "Heavy Drinking from 2017 to 2021 (Ages 12-17)"
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+    
+    return(
+        <>
+        <div className={styles.container}>
+            <Bar data={chartData} options={chartOptions}/>
+        </div>
+        </>
+    )
+}
