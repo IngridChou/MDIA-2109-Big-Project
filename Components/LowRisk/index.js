070faa48fb2e4351ff956def565e3a383db3f944
IngridChou <ingridchoudj@gmail.com>
2023-05-15 19:28:48 -0700

    making improvements

--- Components/LowRisk/index.js
+++ Components/LowRisk/index.js
@@ -5,7 +5,19 @@ import styles from "./LowRisk.module.css"
 export default function LowRisk() {
     return (
         <>
-            <Image src="/result/low-risk.svg" alt="risk level" width={300} height={100} />
+            <div className={styles.main}>
+                <p>You are at</p>
+                <Image src="/result/low-risk.svg" alt="risk level" width={300} height={100} />
+                <p>of over dosing</p>
+            </div>
+            <div className={styles.instructions}>
+                <h2>What should you do?</h2>
+                <ul>
+                    <li>Call emergency services immediately. Provide them your location and follow their instructions carefully</li>
+                    <li>Administer naloxone. Follow the instructions on the package or or ask for guidance from emergency services</li>
+                    <li>Place them in the recovery position to prevent them from choking on vomit</li>
+                </ul>
+            </div>
         </>
     )
 }
